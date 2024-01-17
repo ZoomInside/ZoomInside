b97f69300578bf9ecc489e5c7b71900134ee5621
ZoomInside <zoominsideapp@gmail.com>
2024-01-17 10:33:07 +0200

    Accessing firebase database

---
+++ ZoomInside/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using System.Collections.ObjectModel;
+
+namespace ZoomInside
+{
+    public partial class MainPage : ContentPage
+    {        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        FirebaseClient firebaseClient = new FirebaseClient("https://zoominside-2ccf3-default-rtdb.europe-west1.firebasedatabase.app/");
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            firebaseClient.Child("Todo").PostAsync(new TodoItem
+            {
+                Title = TitleEntry.Text,
+            });
+        }
+    }
+}
